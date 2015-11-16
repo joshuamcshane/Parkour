3cdf4a6e220bb5bd3f021e0aef0cf57257186f47
joshuamcshane <joshuamcshane@icloud.com>
2015-11-15 20:11:04 -0500

    Notification permissions

---
+++ Parkour/PenaltyAlert.swift
@@ -0,0 +1,13 @@
+//
+//  PenaltyAlert.swift
+//  Parkour
+//
+//  Created by Joshua McShane on 11/15/15.
+//  Copyright © 2015 Nathan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class PenaltyAlert: UILocalNotification {
+
+}
